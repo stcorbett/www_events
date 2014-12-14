428d0f61943fc1375b23118c568a9ad5ea8a5a07
Sean Corbett <stcorbett@gmail.com>
2014-12-14 16:02:34 -0600

    ignore authentication config file

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "906452869379104", "eddac7e4f6dab30d06f8337cd1e8814f"
-end
