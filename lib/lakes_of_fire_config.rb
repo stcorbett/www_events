160779bfcd2cd5d5cd5c8e048bfdc4782771e885
Sean Corbett <stcorbett@gmail.com>
2015-02-01 21:22:31 -0500

    add details to lakes of fire config

--- lib/lakes_of_fire_config.rb
+++ lib/lakes_of_fire_config.rb
@@ -9,5 +9,21 @@ class LakesOfFireConfig
       Time.zone.local(2015, 6, 21, 12, 0)
     end
 
+    def event_days
+      {
+        wednesday:  Date.new(2015, 6, 17),
+        thursday:   Date.new(2015, 6, 18),
+        friday:     Date.new(2015, 6, 19),
+        saturday:   Date.new(2015, 6, 20),
+        sunday:     Date.new(2015, 6, 21)
+      }
+    end
+
+    def event_day_names
+      event_days.values.collect do |day|
+        day.strftime("%A")
+      end
+    end
+
   end
 end
