dc17d45f9f9d5fca7fa1b35e194aafde33ad694b
Sean Corbett <stcorbett@gmail.com>
2023-05-14 21:28:32 -0500

    updates for rails 7

--- config/application.rb
+++ config/application.rb
@@ -1,34 +1,22 @@
-require File.expand_path('../boot', __FILE__)
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module WwwEvents
+module Application
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Eastern Time (US & Canada)'
-    config.active_record.default_timezone = :local
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    config.autoload_paths += %W(#{config.root}/lib)
-
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
-end
-
-class Application < Rails::Application
-  site_config = YAML.load_file('config/locales/en.yml') rescue {}
-
-  config.time_zone = site_config["en"]["event_time_zone"]
-  require 'lakes_of_fire_config.rb'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220608185819) do
+ActiveRecord::Schema[6.1].define(version: 20220608185819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
