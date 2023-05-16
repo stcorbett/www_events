d7fcd2a485bedba17d15367b7fffc53c6bb4f5f8
Sean Corbett <stcorbett@gmail.com>
2023-05-16 18:35:25 -0500

    Revert "Update lakes_of_fire_config.rb"
    This reverts commit c8ae3ebd23b5fa78261af7aeed6f4ef080668ca3.

--- lib/lakes_of_fire_config.rb
+++ lib/lakes_of_fire_config.rb
@@ -1,52 +1,52 @@
 class LakesOfFireConfig
   class << self
 
-    # def year
-    #   I18n.t(:year)
-    # end
-    #
-    # def event_submissions_close_at
-    #   Time.zone.parse( I18n.t(:submissions_close_time) )
-    # end
-    #
-    # def start_date
-    #   Date.parse( I18n.t(:start_date) )
-    # end
-    #
-    # def start_time
-    #   Time.zone.parse( "#{I18n.t(:start_time_hour)}:00", start_date )
-    # end
-    #
-    # def end_date
-    #   Date.parse( I18n.t(:end_date) )
-    # end
-    #
-    # def end_time
-    #   Time.zone.parse( "#{I18n.t(:end_time_hour)}:00", end_date )
-    # end
-    #
-    # def event_days
-    #   {
-    #     wednesday:  start_date,
-    #     thursday:   start_date + 1,
-    #     friday:     start_date + 2,
-    #     saturday:   start_date + 3,
-    #     sunday:     end_date
-    #   }
-    # end
-    #
-    # def event_day_names
-    #   event_days.values.collect do |day|
-    #     day.strftime("%A")
-    #   end
-    # end
-    #
-    # def burn_times_filter
-    #   {
-    #     burn_start: "#{I18n.t(:start_date)} #{I18n.t(:start_time_hour)}:00",
-    #     burn_end: "#{I18n.t(:end_date)} #{I18n.t(:end_time_hour)}:00"
-    #   }
-    # end
+    def year
+      I18n.t(:year)
+    end
+
+    def event_submissions_close_at
+      Time.zone.parse( I18n.t(:submissions_close_time) )
+    end
+
+    def start_date
+      Date.parse( I18n.t(:start_date) )
+    end
+
+    def start_time
+      Time.zone.parse( "#{I18n.t(:start_time_hour)}:00", start_date )
+    end
+
+    def end_date
+      Date.parse( I18n.t(:end_date) )
+    end
+
+    def end_time
+      Time.zone.parse( "#{I18n.t(:end_time_hour)}:00", end_date )
+    end
+
+    def event_days
+      {
+        wednesday:  start_date,
+        thursday:   start_date + 1,
+        friday:     start_date + 2,
+        saturday:   start_date + 3,
+        sunday:     end_date
+      }
+    end
+
+    def event_day_names
+      event_days.values.collect do |day|
+        day.strftime("%A")
+      end
+    end
+
+    def burn_times_filter
+      {
+        burn_start: "#{I18n.t(:start_date)} #{I18n.t(:start_time_hour)}:00",
+        burn_end: "#{I18n.t(:end_date)} #{I18n.t(:end_time_hour)}:00"
+      }
+    end
 
   end
 end
