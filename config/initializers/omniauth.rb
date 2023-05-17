7f1a737fb80272cc49c0eb72af0ae043fa0237a2
Sean Corbett <stcorbett@gmail.com>
2023-05-16 19:56:20 -0500

    remove hardcoded configs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,14 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 # Load application ENV vars and merge with existing ENV vars. Loaded here so can use values in initializers.
-# configs = YAML.load_file('config/application.yml')[Rails.env] rescue {}
-
-configs = {
-  facebook_app_secret: '2c09b7c34757d0c19314cb791de15ea6',
-  facebook_app_id: '906452869379104',
-  google_client_id: '16222360144-kusl4truo9v2l3mouqndpkh4u9ftd675.apps.googleusercontent.com',
-  google_client_secret: 'fyL1xLT6SbmCY8dcUM2-Xqcb'
-}
+configs = YAML.load_file('config/application.yml')[Rails.env] rescue {}
 
 configs.merge!(ENV)
 
